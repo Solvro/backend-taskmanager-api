e87f2939cd6807688c919d0eb91c7b7055f66cf4
ikaros_22 <marek.kedzia@arcsoftware.it>
2023-06-26 23:07:09 +0200

    fix error

--- src/utils/error/error.module.ts
+++ src/utils/error/error.module.ts
@@ -51,7 +51,7 @@ export class InternalServerError implements AppError {
 export enum Resource {
   PROJECT = "PROJECT",
   TASK = "TASK",
-  ASSIGNMENT = "ASSIGNMENT"
+  ASSIGNMENT = "ASSIGNMENT",
   USER_ID = "USER_ID",
   SECRET_KEY = "SECRET_KEY",
   AUTH_DATA = "AUTH_DATA",
