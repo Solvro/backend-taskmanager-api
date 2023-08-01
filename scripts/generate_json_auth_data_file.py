9e9b838f3045724d2684e51b519a2a29d44f56e4
Marek Kedzia <marek.kedzia1@o2.pl>
2023-08-01 22:05:15 +0200

    refactor algorithms

--- scripts/generate_json_auth_data_file.py
+++ scripts/generate_json_auth_data_file.py
@@ -4,8 +4,10 @@ import random
 import string
 import datetime
 
+def generate_random_string(number_of_signs=20):
+    return ''.join(random.choices(string.ascii_lowercase, k=number_of_signs))
 
-def generate_json(users_count, version):
+def generate_json(users_count=15, version=1, key_length=128):
     data = {}
     data["version"] = version
     data["id"] = str(uuid.uuid4())
@@ -13,14 +15,12 @@ def generate_json(users_count, version):
     data["usersCredentials"] = {}
 
     for i in range(1, users_count + 1):
-        username = f"user{i}"
-        random_string = ''.join(random.choices(string.ascii_lowercase, k=20))
-        data["usersCredentials"][username] = random_string
+        username = f"u_{generate_random_string()}"
+        data["usersCredentials"][username] = generate_random_string(key_length)
 
-    return data
+    with open('created_auth_credentials.json', "w") as file:
+        json.dump(data, file, indent=2)
 
 
-json_data = generate_json(5, 2)
-
-with open('created_auth_credentials.json', "w") as file:
-    json.dump(json_data, file, indent=2)
+if(__name__ == "__main__"):
+    generate_json()
